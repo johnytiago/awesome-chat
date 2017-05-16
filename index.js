bb5e705103665520209a37a5fcd96437e9bbc3c8
johnytiago <joaosao10@gmail.com>
2017-05-16 10:36:46 +0100

    1st Step: Hello World Server

--- index.js
+++ index.js
@@ -1,53 +1,20 @@
 'use strict';
 const Hapi = require('hapi');
-const routes = require('./server/routes');
-const Inert = require('inert');
-const Vision = require('vision');
-const server = new Hapi.Server();
-const messages = require('./server/messages');
-server.connection({ port: 3000, host: 'localhost' });
 
-const io = require('socket.io')(server.listener);
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-
-  socket.on('chat message', function(msg){
-    console.log(msg);
-    messages.push({
-      user: msg.user,
-      text: msg.text
-    }); 
-    io.emit('chat message', msg);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+server.connection({ 
+  port: 3000, 
+  host: 'localhost' 
 });
 
-server.register([Inert, Vision], (err) => {
-    if (err) {
-        throw err;
-    }
-
-    server.views({
-      engines: {
-        html: require('handlebars')
-      },
-      relativeTo: __dirname,
-      path: './templates',
-      layoutPath: './templates/layout',
-      partialsPath: './templates',
-      layout: 'default'
-    });
-
-    server.route(routes);
-
-    server.start((err) => {
-      if (err) {
-        throw err;
-      }
-      console.log(`Server running at: ${server.info.uri}`);
-    });
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
 });
+
+// TODO
+// Start hello world server
+// Add route
+// Add views
+// (register plugins)
